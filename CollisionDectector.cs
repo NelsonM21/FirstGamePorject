e821282b36c1c75a9de31382bd212647bbc31750
NelsonM21 <98407167+NelsonM21@users.noreply.github.com>
2022-05-03 23:02:49 -0400

    Update CollisionDectector.cs

--- CollisionDectector.cs
+++ CollisionDectector.cs
@@ -1,24 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class CollisionDectector : MonoBehaviour
 {
-    public float hitCounter;
-    // Start is called before the first frame update
+    public float hitCounter = 0;
+    public TextMesh numberText;
+    
+    // Start is called before the first frame update//
     void Start()
     {
-        
+
     }
-    public void OnCollisionEnter(Collision collision)
+    public void OnCollisonEnter(Collision collision, TextOnScreen textOnScreen)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            hitCounter += 1;
+            print("I've been had " + hitCounter + "times");
+            numberText.text = hitCounter.ToString();
+
+            if (hitCounter >= 5)
+            {
+                textOnScreen.whiteCube.SetActive(false);
+            }
+        }
+    }
+    public void OnTriggerEnter(Collider other)
     {
         hitCounter += 1;
-        print("I've been hit " + hitCounter + "times.");
+        print("I smell the smelly smell of someone trying to take me money" + hitCounter + "times.");
     }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
+
